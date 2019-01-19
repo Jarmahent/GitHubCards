2be0994fe5b823902be0820e5de1149c32229066
Kevin Hernandez <kjh7796@gmail.com>
2019-01-18 21:50:52 -0500

    Removed sensitive data

--- routes/getUser.js
+++ routes/getUser.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 const axios = require("axios");
 
-const clientId = "472988b387f54d295169";
-const clientSecret = "34962f4b01e8a2112b82f33b51c191a35ad8e052";
+const clientId = "";
+const clientSecret = "";
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
